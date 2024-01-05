7e9f73f7068355e30ef088cbd7ce9d33f5c2902a
Emre <elci1625@hotmail.com>
2024-01-05 23:54:05 +0300

    Fixed all problems at js file

--- script.js
+++ script.js
@@ -8,10 +8,39 @@ const Gameboard = (() => {
     const getBoard = () => {
       return board;
     };
+
+    const checkTie = () => {
+      return board.every(cell => cell !== '');
+    };
+
+    const resetBoard = () => {
+      board = ['','','','','','','','',''];
+    };
+
+    const checkWin = () => {
+      const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      return winConditions.some(condition => 
+        board[condition[0]] !== '' &&
+        board[condition[0]] === board[condition[1]] &&
+        board[condition[0]] === board[condition[2]]
+      );
+    };
   
     return {
       updateBoard,
-      getBoard
+      getBoard,
+      checkWin,
+      checkTie,
+      resetBoard
     };
 })();
 
@@ -30,16 +59,26 @@ const GameController = ((player1, player2) => {
       Gameboard.updateBoard(index, currentPlayer.mark);
       switchPlayer();
     };
-  
+    
+    const getCurrentPlayer = () => {
+      return currentPlayer;
+    };
+
+    const getWinnerPlayer = () => {
+      return currentPlayer === player1 ? player2 : player1;
+    };
+    
     return {
-      makeMove
+      makeMove,
+      getCurrentPlayer,
+      getWinnerPlayer
     };
 })(Player('Player with X', 'X'), Player('Player with O', 'O'));
 
 const DisplayController = (() => {
     const gameboardContainer = document.querySelector('#gameBoard');
-    const restartButton = document.querySelector('#restart-button');
-    const resultDisplay = document.querySelector('#result-display');
+    const restartButton = document.querySelector('#restartBtn');
+    const resultDisplay = document.querySelector('#resultDisplay');
   
     const renderGameboard = () => {
       gameboardContainer.innerHTML = '';
@@ -58,7 +97,7 @@ const DisplayController = (() => {
   
     const checkGameOver = () => {
       if (Gameboard.checkWin()) {
-        resultDisplay.textContent = `${GameController.getCurrentPlayer().name} wins!`;
+        resultDisplay.textContent = `${GameController.getWinnerPlayer().name} wins!`;
       } else if (Gameboard.checkTie()) {
         resultDisplay.textContent = "It's a tie!";
       }
